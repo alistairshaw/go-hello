edfd33decb0575694d1ce8e8d0b7fd1ce7c53c81
Alistair Shaw <alistairshaw@gmail.com>
2018-12-15 19:52:32 +0000

    Mux router, static assets, templates and layouts

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ WORKDIR /go/src/go-hello
 
 RUN apk add --no-cache git
 RUN go get github.com/codegangsta/gin
+RUN go get github.com/gorilla/mux
 
 ENTRYPOINT gin -a 80 run main.go
--- main.go
+++ main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"flag"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+//ToolPageData : Template variables for the tools page
+type ToolPageData struct {
+	PageTitle    string
+	SelectedTool string
+}
+
+//SpellPageData : Template variables for the spells page
+type SpellPageData struct {
+	PageTitle     string
+	SelectedSpell string
+}
+
 func main() {
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+	var dir string
+	flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+	r := mux.NewRouter()
 
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("./"))
+	r.PathPrefix("/static/").Handler(fs)
 
-    http.ListenAndServe(":80", nil)
+	r.HandleFunc("/tools/{tool}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		data := ToolPageData{
+			PageTitle:    vars["tool"],
+			SelectedTool: vars["tool"]}
+		tmpl := template.Must(template.ParseFiles("web/template/layouts/main.html", "web/template/tool.html"))
+		tmpl.ExecuteTemplate(w, "main", data)
+	})
+
+	r.HandleFunc("/spells/{spell}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		data := SpellPageData{
+			PageTitle:     vars["spell"],
+			SelectedSpell: vars["spell"]}
+		tmpl := template.Must(template.ParseFiles("web/template/layouts/main.html", "web/template/spell.html"))
+		tmpl.ExecuteTemplate(w, "main", data)
+	})
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("web/template/layouts/main.html", "web/template/home.html"))
+		tmpl.ExecuteTemplate(w, "main", nil)
+	})
+
+	http.ListenAndServe(":80", r)
 }
